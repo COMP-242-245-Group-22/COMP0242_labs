0acb3ca677c25cd080d62a55780137a99d436828
Yukiyt778 <135519587+Yukiyt778@users.noreply.github.com>
2024-11-03 01:19:40 +0000

    Update robot_localization_system.py

--- final/robot_localization_system.py
+++ final/robot_localization_system.py
@@ -7,13 +7,15 @@ import matplotlib.pyplot as plt
 class FilterConfiguration(object):
     def __init__(self):
         # Process and measurement noise covariance matrices
-        self.V = np.diag([0.1, 0.1, 0.05]) ** 2  # Process noise covariance
+        # Process noise covariance represents the uncertainty in the system's predicted state, considering that multiple state variables (e.g., x-position, y-position, and orientation) can be interdependent.
+        self.V = np.diag([0.01, 0.01, 0.05]) ** 2  # Process noise covariance, V is the proccess noise
         # Measurement noise variance (range measurements)
-        self.W_range = 0.5 ** 2
-        self.W_bearing = (np.pi * 0.5 / 180.0) ** 2
+        # Single variable for individual senser reading
+        self.W_range = 0.03 ** 2 # distance to landmarks: 0.025m
+        self.W_bearing = (np.pi * 0.3 / 180.0) ** 2 # orientation relative to landmarks: 0.25radian
 
         # Initial conditions for the filter
-        self.x0 = np.array([2.0, 3.0, np.pi / 4])
+        self.x0 = np.array([1.0, 1.0, np.pi / 4])
         self.Sigma0 = np.diag([1.0, 1.0, 0.5]) ** 2
 
 
@@ -25,15 +27,37 @@ class Map(object):
             [10, 15]
         ])
 
+    # Configuaration of a 30x30 rectangular grid of landmarks with 1-meter spacing from the center of robot
+    def use_more_landmarks(self, n=30, spacing=1):
+        self.landmarks = np.array([
+            [i * spacing, j * spacing]
+            for i in range(n)
+            for j in range(n)
+         ])
+    # def use_more_landmarks(self, n=20, spacing=1):
+    #     # Generate landmarks along the line y = x
+    #     self.landmarks = np.array([
+    #         [i * spacing, i * spacing]
+    #         for i in range(n)
+    #     ])
+        # half_width = (width - 1) / 2 * spacing
+        # half_height = (height - 1) / 2 * spacing
+        # landmarks = []
+        # for i in range(width):
+        #     for j in range(height):
+        #         x =  - half_width + i * spacing
+        #         y =  - half_height + j * spacing
+        #         landmarks.append([x, y])
+        # self.landmarks = np.array(landmarks)
 
+        
 class RobotEstimator(object):
-
     def __init__(self, filter_config, map):
         # Variables which will be used
         self._config = filter_config
         self._map = map
 
-    # This nethod MUST be called to start the filter
+    # This method MUST be called to start the filter
     def start(self):
         self._t = 0
         self._set_estimate_to_initial_conditions()
@@ -58,6 +82,7 @@ class RobotEstimator(object):
         return self._x_est, self._Sigma_est
 
     # This method gets called if there are no observations
+    # often due to the limitations or conditions of the robot's sensors or the environment
     def copy_prediction_to_estimate(self):
         self._x_est = self._x_pred
         self._Sigma_est = self._Sigma_pred
@@ -136,17 +161,60 @@ class RobotEstimator(object):
         C = np.array(C)
         y_pred = np.array(y_pred)
 
-        # Innovation. Look new information! (geddit?)
+        # Innovation. Look new information! (get it?)
         nu = y_range - y_pred
 
-        # Since we are oberving a bunch of landmarks
+        # Since we are observing a bunch of landmarks
         # build the covariance matrix. Note you could
         # swap this to just calling the ekf update call
         # multiple times, once for each observation,
         # as well
         W_landmarks = self._config.W_range * np.eye(len(self._map.landmarks))
+
         self._do_kf_update(nu, C, W_landmarks)
 
         # Angle wrap afterwards
         self._x_est[-1] = np.arctan2(np.sin(self._x_est[-1]),
                                      np.cos(self._x_est[-1]))
+        
+    def update_from_landmark_bearing_observations(self, y_bearing):
+        # Predicted the landmark measurements and build up the observation Jacobian
+        y_pred = []
+        C = []
+        x_pred = self._x_pred
+
+        for lm in self._map.landmarks:
+            dx_pred = lm[0] - x_pred[0]
+            dy_pred = lm[1] - x_pred[1]
+            range_pred = np.sqrt(dx_pred**2 + dy_pred**2)
+            
+            # Predicted bearing relative to the robot's orientation
+            bearing_pred = np.arctan2(dy_pred, dx_pred) - x_pred[2]
+            # Normalize bearing to be within [-pi, pi]
+            bearing_pred = np.arctan2(np.sin(bearing_pred), np.cos(bearing_pred))
+            y_pred.append(bearing_pred)
+
+            # Jacobian of the bearing measurement model
+            C_bearing = np.array([
+                -dy_pred / (range_pred ** 2),
+                dx_pred / (range_pred ** 2),
+                -1
+            ])
+            C.append(C_bearing)
+
+        # Convert lists to arrays
+        C = np.array(C)
+        y_pred = np.array(y_pred)
+
+        # Calculate the innovation (measurement residual)
+        nu = y_bearing - y_pred
+        # Normalize the bearing residual to be within [-pi, pi]
+        nu = np.arctan2(np.sin(nu), np.cos(nu))
+
+        # Measurement noise covariance for bearings
+        W_landmarks = self._config.W_bearing * np.eye(len(self._map.landmarks))
+        self._do_kf_update(nu, C, W_landmarks)
+
+        # Angle wrap afterwards for the estimated state
+        self._x_est[-1] = np.arctan2(np.sin(self._x_est[-1]),
+                                    np.cos(self._x_est[-1]))
